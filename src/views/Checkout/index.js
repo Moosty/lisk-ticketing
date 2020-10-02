e6d58b02e305f7080eef347c745c9a730a34016c
jurrem <jurre@moosty.com>
2020-10-02 16:42:51 +0200

    laatste

--- src/views/Checkout/index.js
+++ src/views/Checkout/index.js
@@ -5,9 +5,15 @@ import {TicketList} from "components/TicketList";
 import {AccountHeader} from "components/AccountHeader";
 
 export const Checkout = (props) => {
-    return <div className="mt-10">
-<div></div>
-        <CartBottom
-            totalPrice="€ 185.56"/>
-    </div>;
+  return <div className="mt-10">
+    <div>
+      <ul>
+        <li>Tickets in de basket</li>
+        <li>Totale kosten</li>
+        <li>betalen</li>
+      </ul>
+    </div>
+    <CartBottom
+      totalPrice="€ 185.56"/>
+  </div>;
 };
--- src/views/SignUp/index.js
+++ src/views/SignUp/index.js
@@ -1,21 +1,16 @@
-import React from "react";
-import {Header} from "components/Header";
-import {CartBottom, EventHeader, TicketListItem, TicketType} from "components/index";
-import {TicketList} from "components/TicketList";
-import {AccountHeader} from "components/AccountHeader";
-import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
+import React, {useState} from "react";
+
 import {SignUpUser} from "components/SignUpUser";
 import {SignUpOrg} from "components/SignUpOrg";
 
 
-export const SignUp = (props) => {
-  return <div className="mt-10">
-  <SignUpUser />
-   <SignUpOrg />
-  
-  </div>;
 
+export const SignUp = (props) => {
+  const [state, setState] = useState(<SignUpUser/>);
+
+  return <div className="mt-10">
+    {state}
+   </div>;
 
 
 };
