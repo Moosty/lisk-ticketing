47d8bf69fc869b666245e17b3fb7bebb3ec3f8de
jurrem <jurre@moosty.com>
2020-10-21 10:42:24 +0200

    mytickets aanpassing

--- src/views/MyTickets/index.js
+++ src/views/MyTickets/index.js
@@ -12,14 +12,6 @@ import {ticketStatuses} from "../../store/reducers/blockchain/portfolio.reducer"
 
 export const MyTickets = withReducer("mytickets", reducer)((props) => {
 const { address } = useParams();
-const organiser = useSelector(({blockchain}) => blockchain.organiser.organiserAccounts.address === address);
-
-useEffect( () => {
-  console.log("organiserId params", address);
-  console.log("organiser", organiser);
-  }, organiser,
-
-);
 
 return <div className="mt-10">
 
@@ -31,14 +23,6 @@ return <div className="mt-10">
       <TabsTickets type="user"/>
     </div>}
 
-    {props.type === 'organiser' && <div>
-      <Header
-        title="My Events"
-        subtitle="keep all your tickets safe"
-      />
-      <TabsTickets type="organiser" />
-
-    </div>}
 
   </div>;
 });
