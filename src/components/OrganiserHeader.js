429e53f18e65c1c077f096e7437c16f92eff1ac6
corbifex <sander@moosty.com>
2020-12-09 16:25:25 +0100

    header fix

--- src/components/OrganiserHeader.js
+++ src/components/OrganiserHeader.js
@@ -86,7 +86,7 @@ export const OrganiserHeader = ({name, balance, token, button1, button2, onClick
       <div className="bg-fixed sm:bg-scroll bg-cover"
            style={{backgroundImage: "url(/images/bgTicketing.jpg)", height: "auto"}}>
         <div className="w-full flex-auto h-full" style={{backgroundColor: "#1a202c94"}}>
-          <div className=" p-8 text-centeralign-middle h-full flex flex-col justify-between">
+          <div className=" p-8 text-center align-middle h-full flex flex-col justify-between">
             <h1 className="text-4xl leading-10 text-white font-extrabold">{name}</h1>
             <div  className="text-xl text-center text-white">{balance} {token}</div>
 
